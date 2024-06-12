45830d005484cd0e388d768d6f3f983cd626a873
itellaetxe <inigotellaetxe@gmail.com>
2024-06-12 11:37:58 +0200

    REF: Removes whitespaces, makes variabl names more descriptive

--- src/ct2mni152/ct_to_mni152.py
+++ src/ct2mni152/ct_to_mni152.py
@@ -12,7 +12,7 @@ HELP_MESSAGE = ("Usage:\n"
                "[-sdr <SCAN_DEVICE_REMOVAL>(Default: True)]")
 
 
-def setup():
+def arg_setup() -> argparse.Namespace:
     # Argument parser
     arg_parser = argparse.ArgumentParser(
         description="CT to MNI152 Registration",
@@ -30,26 +30,27 @@ def setup():
                             metavar="DIR",
                             help="Path to save the results folder.",
                             required=True)
-    arg_parser.add_argument("-nL", "--no_lesion_path",
+    arg_parser.add_argument("-l", "--lesion_mask_path",
                             metavar="DIR",
-                            help="Path to the no lesion mask.",
+                            help="Path to the lesion mask.",
                             required=True)
     # Parse arguments
     args = arg_parser.parse_args()
-    
+
     # Check that arguments are correct
     if not os.path.exists(args.ct_scan_path):
-        raise ValueError("CT scan path does not exist.")
+        raise ValueError(f"CT scan path does not exist. (Could not find: {args.ct_scan_path})")
     if not os.path.exists(args.output_path):
-        raise ValueError("Output path does not exist.")
+        raise ValueError(f"Output path does not exist. (Could not find: {args.output_path})")
     if not isinstance(args.scan_device_removal, bool):
         raise ValueError("Scan device removal must be a boolean. Write 'True' or 'False'.")
-    
+
     return args
 
 
-def main():
-    if len(sys.argv) == 1:
+def register_ct_and_lesion_to_mni152() -> None:
+    # Print CLI help message if no arguments are provided or if "-h" or "--help" is provided
+    if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
         print(HELP_MESSAGE)
         sys.exit(1)
 
@@ -58,28 +59,29 @@ def main():
     MNI152_T1_PATH = data.MNI152_T1_PATH
     BSPLINE_PATH = data.BSPLINE_PATH
     print(("Location of MNI152 bone image:" , MNI152_BONE_PATH))
-    
-    # Argument parser configuration
-    args = setup()
 
+    # Get CLI input arguments
+    args = arg_setup()
+
+    # Define CT scan, affine matrix, and lesion mask paths.
     ct_scan_path = args.ct_scan_path
-    no_lesion_path = args.no_lesion_path
-    affine_matrix_name = ct_scan_path[:ct_scan_path.find(".nii.gz")]+"_affine.mat"
+    lesion_mask_path = args.no_lesion_path
+    affine_matrix_path = ct_scan_path[:ct_scan_path.find(".")]+"_affine.mat"
 
-    # if you want to do ct scan removal 
+    # If you want to do ct scan removal 
     if "scan_device_removal" in vars(args):
-        ct_scan_wodevice = ct_tools.remove_ct_scan_device(ct_scan_path)
+        ct_scan_no_device = ct_tools.remove_ct_scan_device(ct_scan_path)
     else:
-        ct_scan_wodevice = ct_scan_path
-    
-    # Extract bone from CT scan
-    ct_scan_wodevice_bone = ct_tools.bone_extracted(ct_scan_wodevice)
+        ct_scan_no_device = ct_scan_path
+
+    # Extract skull from CT scan
+    ct_scan_no_device_skull = ct_tools.extract_skull(ct_scan_no_device)
 
     # Register bone from CT scan to MNI152-bone. RIGID REGISTRATION
     call(["flirt",
-          "-in", ct_scan_wodevice_bone,
+          "-in", ct_scan_no_device_skull,
           "-ref", MNI152_BONE_PATH,
-          "-omat", affine_matrix_name,
+          "-omat", affine_matrix_path,
           "-bins", "256",
           "-searchrx", "-180", "180",
           "-searchry", "-180", "180",
@@ -88,30 +90,30 @@ def main():
           "-interp", "trilinear"])
 
     call(["flirt",
-    "-in", ct_scan_wodevice,
+    "-in", ct_scan_no_device,
     "-ref", MNI152_T1_PATH,
     "-applyxfm",
-    "-init", affine_matrix_name,
-    "-out", ct_scan_wodevice[:ct_scan_wodevice.find(".nii.gz")]+"_MNI152.nii.gz"])
+    "-init", affine_matrix_path,
+    "-out", ct_scan_no_device[:ct_scan_no_device.find(".")]+"_MNI152.nii.gz"])
 
     # Perform contrast stretching in the ct scan without device
-    ct_scan_wodevice_contrast_stretching = ct_tools.contrast_stretch(ct_scan_wodevice)
+    ct_scan_wodevice_contrast_stretching = ct_tools.contrast_stretch(ct_scan_no_device)
 
     # Deformable Registration
     call(["flirt",
     "-in", ct_scan_wodevice_contrast_stretching,
     "-ref", MNI152_T1_PATH,
     "-applyxfm",
-    "-init", affine_matrix_name,
-    "-out", ct_scan_wodevice_contrast_stretching[:ct_scan_wodevice_contrast_stretching.find(".nii.gz")]+"_MNI152.nii.gz"])
+    "-init", affine_matrix_path,
+    "-out", ct_scan_wodevice_contrast_stretching[:ct_scan_wodevice_contrast_stretching.find(".")]+"_MNI152.nii.gz"])
 
     call(["elastix",
-          "-m", ct_scan_wodevice_contrast_stretching[:ct_scan_wodevice_contrast_stretching.find(".nii.gz")]+"_MNI152.nii.gz",
+          "-m", ct_scan_wodevice_contrast_stretching[:ct_scan_wodevice_contrast_stretching.find(".")]+"_MNI152.nii.gz",
           "-f", MNI152_T1_PATH,
-          "-mMask", no_lesion_path,
+          "-mMask", lesion_mask_path,
           "-out", os.path.dirname(ct_scan_path),
           "-p", BSPLINE_PATH])
 
 
 if __name__ == "__main__":
-    main()
+    register_ct_and_lesion_to_mni152()
--- src/ct2mni152/main.py
+++ src/ct2mni152/main.py
@@ -1,8 +1,8 @@
 import ct2mni152.ct_to_mni152 as ct_to_mni152
 
 def main():
-    ct_to_mni152.main()
+    ct_to_mni152.register_ct_and_lesion_to_mni152()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # Can also be run as a script
     main()
